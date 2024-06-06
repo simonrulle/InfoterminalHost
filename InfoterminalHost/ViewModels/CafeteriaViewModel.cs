2e4dbd299b8b0d39fa7133b7a20e5c120c3c97f6
Simon_Rullmann <simonrullmann@gmail.com>
2024-06-06 14:02:45 +0200

    [Feature] added ComboBox for date changes

--- InfoterminalHost/ViewModels/CafeteriaViewModel.cs
+++ InfoterminalHost/ViewModels/CafeteriaViewModel.cs
@@ -1,23 +1,44 @@
-﻿using InfoterminalHost.Interfaces;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using InfoterminalHost.Interfaces;
 using InfoterminalHost.Models;
 using InfoterminalHost.Services;
+using Microsoft.UI;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Media;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace InfoterminalHost.ViewModels
 {
-    public partial class CafeteriaViewModel
+    public partial class CafeteriaViewModel : ObservableObject
     {
         private ICafeteriaDataService _cafeteriaDataService;
 
         private MealPlan mealPlan;
 
+        [ObservableProperty]
+        private ObservableCollection<Offering> offerings = new ObservableCollection<Offering>();
+
+        [ObservableProperty]
+        private bool isLoading;
+
+        [ObservableProperty]
+        private string selectedItem;
+
+        [ObservableProperty]
+        private ObservableCollection<string> daten = new ObservableCollection<string>();
+
         public CafeteriaViewModel(ICafeteriaDataService cafeteriaDataService) 
-        { 
+        {
+            isLoading = true;
             _cafeteriaDataService = cafeteriaDataService;
+            Daten = new ObservableCollection<string> { "Item1", "Item2", "Item3" };
+            SelectedItem = daten[0];
             PopulateData();
         }
 
@@ -26,12 +47,29 @@ namespace InfoterminalHost.ViewModels
             try
             {
                 mealPlan = await _cafeteriaDataService.GetMealPlan();
+
+                foreach (Offering offering in mealPlan.Offering) 
+                {
+                    Offerings.Add(offering);
+                }
+
+                IsLoading = false;
             }
             catch (Exception ex) 
             {
                 Console.WriteLine($"Fehler beim Abrufen oder Verarbeiten der JSON-Daten: {ex.Message}");
+
+                IsLoading = false;
             }
         }
 
+        public void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var comboBox = sender as ComboBox;
+            if (comboBox != null && comboBox.SelectedItem != null)
+            {
+                SelectedItem = comboBox.SelectedItem as string;
+            }
+        }
     }
 }
